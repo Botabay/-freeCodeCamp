ff9bbb77e200ffa486c29c431b6618481d5c6d7f
Botabay <tohgl49@gmail.com>
2023-02-27 19:00:16 +0600

    initialized Variables

--- 01.js
+++ 01.js
@@ -7,7 +7,19 @@
    console.log(a,b)
 */
 
-var a=9;
+// var a=9;
 
-//Declare String Variables
-var myFirstName='MyName', myLastName='MyLastName';
+// //Declare String Variables
+// var myFirstName='MyName', myLastName='MyLastName';
+
+
+//Uninitialized Variables
+// Only change code below this line
+var a=5;
+var b=10;
+var c="I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
