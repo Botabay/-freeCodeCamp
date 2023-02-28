75219282e56567537a90ed073f5482eee217903e
Botabay <tohgl49@gmail.com>
2023-02-28 11:17:17 +0600

    applied camalCase

--- 01.js
+++ 01.js
@@ -15,11 +15,22 @@
 
 //Uninitialized Variables
 // Only change code below this line
-var a=5;
-var b=10;
-var c="I am a";
-// Only change code above this line
+// var a=5;
+// var b=10;
+// var c="I am a";
+// // Only change code above this line
 
-a = a + 1;
-b = b + 5;
-c = c + " String!";
+// a = a + 1;
+// b = b + 5;
+// c = c + " String!";
+
+//camalCase
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
