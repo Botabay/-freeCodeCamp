e7b4ea3e3da6ed3f60d1044afcda6758db87ef1f
Botabay <tohgl49@gmail.com>
2023-02-28 22:31:07 +0600

    Subtracted One Number from Another with JavaScript

--- 01.js
+++ 01.js
@@ -45,5 +45,8 @@
 // fact = "is awesome!";
 // console.log(FCC, fact); // Change this line
 
-//Add Two Numbers with JavaScript
-const sum = 10 + 10;
+// //Add Two Numbers with JavaScript
+// const sum = 10 + 10;
+
+//Subtract One Number from Another with JavaScript
+const difference = 45 -33;
