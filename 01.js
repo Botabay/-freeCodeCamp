59a4b275276737e50dba32b45c88f666e842954b
Botabay <tohgl49@gmail.com>
2023-03-02 11:37:08 +0600

    divide

--- 01.js
+++ 01.js
@@ -67,6 +67,10 @@
 // // Only change code below this line
 // myVar--;
 
-const ourDecimal = 5.7;
-// Only change code below this line
-let myDecimal=5.7;
+// const ourDecimal = 5.7;
+// // Only change code below this line
+// let myDecimal=5.7;
+
+//const product = 2.0 * 2.5;
+
+const quotient = 4.4 / 2.0; // Change this line
