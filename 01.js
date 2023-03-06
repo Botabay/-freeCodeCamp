042a49dbeb284355b5593f86d4f3b3563ca500d2
Botabay <tohgl49@gmail.com>
2023-03-06 11:40:44 +0600

    task "if to switch"

--- 01.js
+++ 01.js
@@ -135,19 +135,38 @@
 //    return x*5;
 // }
 
-function caseInSwitch(val) {
+// function caseInSwitch(val) {
+//    let answer = "";
+//    // Only change code below this line
+//    switch(val){
+//      case 1: return 'alpha';
+//              break;
+//      case 2: return 'beta'
+//              break;
+//      case 3: return 'gamma'
+//              break;
+//      case 4: return 'delta'
+//    } 
+//    // Only change code above this line
+//    return answer;
+//  } 
+//  caseInSwitch(1);
+
+function chainToSwitch(val) {
    let answer = "";
    // Only change code below this line
    switch(val){
-     case 1: return 'alpha';
-             break;
-     case 2: return 'beta'
-             break;
-     case 3: return 'gamma'
-             break;
-     case 4: return 'delta'
-   } 
+   case "bob": answer = "Marley";
+     break;
+   case 42: answer = "The Answer";
+     break;
+   case 1: answer = "There is no #1";
+     break;
+   case 99: answer = "Missed me by this much!";
+     break;
+   case 7: answer = "Ate Nine";
+   }
    // Only change code above this line
-   return answer;
- } 
- caseInSwitch(1);
+   return answer;}
+ 
+ chainToSwitch(7);
