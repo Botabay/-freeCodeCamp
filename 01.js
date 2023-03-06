bf4e606af5b7c25baa93c90c5550147a9717e102
Botabay <tohgl49@gmail.com>
2023-03-06 11:26:23 +0600

    task "switch"

--- 01.js
+++ 01.js
@@ -131,6 +131,23 @@
 // functionWithArgs(1,2)
 
 // created function of multiply
-function timesFive(x){
-   return x*5;
-}
+// function timesFive(x){
+//    return x*5;
+// }
+
+function caseInSwitch(val) {
+   let answer = "";
+   // Only change code below this line
+   switch(val){
+     case 1: return 'alpha';
+             break;
+     case 2: return 'beta'
+             break;
+     case 3: return 'gamma'
+             break;
+     case 4: return 'delta'
+   } 
+   // Only change code above this line
+   return answer;
+ } 
+ caseInSwitch(1);
