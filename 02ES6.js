9973100ce72b01fa758392227efe0fa06524b99e
Botabay <tohgl49@gmail.com>
2023-03-09 13:01:26 +0600

    task "Mutate an Array Declared with const"

--- 02ES6.js
+++ 02ES6.js
@@ -8,3 +8,13 @@ function checkScope() {
     console.log('Function scope i is: ', i);
     return i;
   }
+
+//Mutate an Array Declared with const
+const s = [5, 7, 2];
+function editInPlace() {
+  // Only change code below this line
+  s.sort();
+  // Using s = [2, 5, 7] would be invalid
+  // Only change code above this line
+}
+editInPlace();
