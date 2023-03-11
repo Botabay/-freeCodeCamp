a0453cccd0d1acfbd35ef57e7f1c0e45f84a9fb8
Botabay <tohgl49@gmail.com>
2023-03-11 11:54:15 +0600

    task "Use Destructuring Assignment to Assign Variables from Nested Objects"

--- 02ES6.js
+++ 02ES6.js
@@ -53,3 +53,13 @@ const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
 let arr2;
 arr2 = [...arr1];  // Change this line
 console.log(arr2);
+
+//Use Destructuring Assignment to Assign Variables from Nested Objects
+const LOCAL_FORECAST = {
+  yesterday: { low: 61, high: 75 },
+  today: { low: 64, high: 77 },
+  tomorrow: { low: 68, high: 80 }
+};
+// Only change code below this line
+  const {today:{low:lowToday,high:highToday}}=LOCAL_FORECAST;
+// Only change code above this line
